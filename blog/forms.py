e9c1867e418cff112003aa485ff6cbc29beb9c06
MarcinSachs <marcinsachs@gmail.com>
2025-08-25 15:55:40 +0200

    Add login form

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, TextAreaField
+from werkzeug.routing import ValidationError
+from wtforms import StringField, SubmitField, BooleanField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired
-
+from config import Config
 
 class EntryForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     body = TextAreaField("Body", validators=[DataRequired()])
     is_published = BooleanField("Publish", default=True)
     submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+
+   username = StringField('Username', validators=[DataRequired()])
+   password = PasswordField('Password', validators=[DataRequired()])
+
+   def validate_username(self, field):
+       if field.data != Config.ADMIN_USERNAME:
+           raise ValidationError("Invalid username")
+       return field.data
+
+   def validate_password(self, field):
+       if field.data != Config.ADMIN_PASSWORD:
+           raise ValidationError("Invalid password")
+       return field.data
--- blog/routes.py
+++ blog/routes.py
@@ -1,9 +1,10 @@
 
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, session
 from blog import app
 from blog.forms import EntryForm
 from blog.models import Entry, db
 
+from blog.forms import LoginForm
 
 @app.route("/")
 def index():
@@ -41,3 +42,26 @@ def create_or_edit_entry(entry_id=None):
         return redirect(url_for("index"))
     # Przekazujemy entry_id do szablonu
     return render_template("entry_form.html", form=form, entry_id=entry_id)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))

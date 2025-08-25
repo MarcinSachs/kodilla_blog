2e3370f9eb1b80eef62bed304f1f61f5a7a2dcd8
Marcin Sachs <marcinsachs@gmail.com>
2025-08-25 19:32:59 +0200

    Add permissions

--- blog/routes.py
+++ blog/routes.py
@@ -5,6 +5,17 @@ from blog.forms import EntryForm
 from blog.models import Entry, db
 
 from blog.forms import LoginForm
+import functools
+
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
+
 
 @app.route("/")
 def index():
@@ -15,6 +26,7 @@ def index():
 
 @app.route("/new-post", methods=["GET", "POST"])
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def create_or_edit_entry(entry_id=None):
     if entry_id:  # Edycja istniejącego posta
         # Używamy get_or_404 zamiast filter_by.first_or_404()
@@ -43,25 +55,26 @@ def create_or_edit_entry(entry_id=None):
     # Przekazujemy entry_id do szablonu
     return render_template("entry_form.html", form=form, entry_id=entry_id)
 
+
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
-   form = LoginForm()
-   errors = None
-   next_url = request.args.get('next')
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           session['logged_in'] = True
-           session.permanent = True  # Use cookie to store session.
-           flash('You are now logged in.', 'success')
-           return redirect(next_url or url_for('index'))
-       else:
-           errors = form.errors
-   return render_template("login_form.html", form=form, errors=errors)
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
 
 
 @app.route('/logout/', methods=['GET', 'POST'])
 def logout():
-   if request.method == 'POST':
-       session.clear()
-       flash('You are now logged out.', 'success')
-   return redirect(url_for('index'))
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
